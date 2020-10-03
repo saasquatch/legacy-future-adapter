02c68a7d8621aea1772d8fff7dfda99f1070a84c
slisaasquatch <sli@saasquat.ch>
2020-10-02 21:03:00 -0700

    simply TestHelper

--- src/test/java/com/saasquatch/legacyfutureadapter/TestHelper.java
+++ src/test/java/com/saasquatch/legacyfutureadapter/TestHelper.java
@@ -1,8 +1,6 @@
 package com.saasquatch.legacyfutureadapter;
 
 import java.time.Duration;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -10,30 +8,17 @@ import com.google.common.util.concurrent.SettableFuture;
 
 public class TestHelper {
 
-  public static Executor threadPerTaskExecutor() {
-    return r -> new Thread(r).start();
-  }
-
-  public static <T> CompletableFuture<T> delayedCompletableFuture(@Nullable T elem,
-      @Nonnull Duration delay) {
-    return CompletableFuture.supplyAsync(() -> {
+  public static <T> ListenableFuture<T> delayedFuture(@Nullable T elem, @Nonnull Duration delay) {
+    final SettableFuture<T> f = SettableFuture.create();
+    final Thread t = new Thread(() -> {
       try {
         Thread.sleep(delay.toMillis());
       } catch (InterruptedException e) {
       }
-      return elem;
-    }, threadPerTaskExecutor());
-  }
-
-  public static <T> ListenableFuture<T> delayedFuture(@Nullable T elem, @Nonnull Duration delay) {
-    final SettableFuture<T> f = SettableFuture.create();
-    delayedCompletableFuture(null, delay).whenComplete((_ignored, t) -> {
-      if (t != null) {
-        f.setException(t);
-      } else {
-        f.set(elem);
-      }
+      f.set(elem);
     });
+    t.setDaemon(true);
+    t.start();
     return f;
   }
 
